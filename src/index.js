4ce2b103ddc645ca9796cf29bb5b5ff4cd9a9df9
itc-s24020 <s24020@std.it-college.ac.jp>
2025-10-02 10:18:56 +0900

    演習問題

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-// ここに処理を記述する
+// 追加したときに実行される関数
+const onClickAddButton = () => {
+  // テキストボックスの値を取得し初期化する
+  const inputElement = document.getElementById("add-text");
+  const text = inputElement.value;
+  inputElement.value = ""; // テキストボックスをからにする
+
+  console.log(text);
+
+  // メモ一覧に追加する要素を作成
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+
+  // メモの内容を設定
+  const p = document.createElement("p");
+  p.innerText = text;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  // 削除ボタンのイベント
+  deleteButton.addEventListener("click", () => {
+    li.remove();
+  });
+
+  // メモ一覧に追加
+  div.appendChild(p);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+  document.getElementById("memo-list").appendChild(li);
+};
+
+// 追加ボタンを取得して、クリックイベントを設定
+document
+  .getElementById("add-button")
+  .addEventListener("click", onClickAddButton);
