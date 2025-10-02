7e2b3653200b651edb058be56af9e75259571cf0
itc-s24008 <167943996+itc-s24008@users.noreply.github.com>
2025-10-02 11:42:40 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
+// ここに処理を記述する
+
 const onClickAddButton = () => {
-   const text = document.getElementById("add-text").value;
-   document.getElementById("add-text").value = "";
+  const text = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = ""; 
+  //console.log(text);
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  p.innerText = text;
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
 
- const li = document.createElement("li");
- const div = document.createElement("div");
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("memo-list").removeChild(deleteTarget);
+  });
+  div.appendChild(p); 
+  div.appendChild(deleteButton); 
+  li.appendChild(div); 
+  document.getElementById("memo-list").appendChild(li);
+};
 
- const p = document.createElement("p");
- p.innerText = text;
-
- const deleteButton = document.createElement("button");
-   deleteButton.innerText = "削除";
-   deleteButton.addEventListener("click", () => {
-    document.getElementById("task-list").removeChild(li);
-   });
-
- div.appendChild(p);
- div.appendChild(deleteButton);
- li.appendChild(div);
-   document.getElementById("task-list").appendChild(li);
-}
+document
+  .getElementById("add-button")
+  .addEventListener("click", onClickAddButton);
