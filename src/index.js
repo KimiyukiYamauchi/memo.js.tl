c505266edb18a0f5f5c55c94fa350ee496dc57af
itc-ss24007 <s24007@std.it-college.ac.jp>
2025-10-02 09:42:36 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// ここに処理を記述する
+//[追加]ボタンを押し時に実行する関数
+const onClickAddButton = () => {
+  // テキストボックスの値を取得し、変数に格納
+  const inputText = document.getElementById("add-text").value;
+  console.log(inputText);
+  //メモー一覧に追加する要素を作成する
+  //メモーの内容を設定する
+  //削除ボタンを作成し、クリックイベントを設定する
+  //メモー一覧に追加する
+};
+// 追加ボタンを取得して、クリックイベントを設定
+document
+  .getElementById("add-button")
+  .addEventListener("click", onClickAddButton);
