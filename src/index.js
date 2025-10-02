04bcd1ab6529eabf328e53fa2d57a2470a989831
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-10-02 10:47:34 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// ここに処理を記述する
+const onClickAddButton = () => {
+   const text = document.getElementById("add-text").value;
+   document.getElementById("add-text").value = "";
+
+ const li = document.createElement("li");
+ const div = document.createElement("div");
+
+ const p = document.createElement("p");
+ p.innerText = text;
+
+ const deleteButton = document.createElement("button");
+   deleteButton.innerText = "削除";
+   deleteButton.addEventListener("click", () => {
+    document.getElementById("task-list").removeChild(li);
+   });
+
+ div.appendChild(p);
+ div.appendChild(deleteButton);
+ li.appendChild(div);
+   document.getElementById("task-list").appendChild(li);
+}
