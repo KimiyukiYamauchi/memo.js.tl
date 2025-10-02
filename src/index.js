46e2b1c09856c54659b26f80b8b34de20d10968c
itc-s24021 <s24021@std.it-college.ac.jp>
2025-10-02 12:34:15 +0900

    課題

--- src/index.js
+++ src/index.js
@@ -1,36 +1,39 @@
-//追加ボタンの挙動
+// 追加ボタン押下時に実行する関数
 const onClickAddButton = () => {
-  //テキストボックス取得
-  const textEl = document.getElementById("add-text");
-  //テキストボックス内の文字を取得
-  const text = textEl.value;
-  //テキストボックスを空にする
-  textEl.value = "";
-  //メモ一覧に追加する要素の作成
+  // テキストボックスの値を取得し、初期化
+  const text = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = ""; //テキストボックスを空にする
+  console.log(text);
+
+  // メモ一覧に追加する要素を作成
   const li = document.createElement("li");
   const div = document.createElement("div");
+
+  // メモの内容を設定する
   const p = document.createElement("p");
-  //メモの内容をpタグに追加
-  p.textContent = text;
-  //削除ボタンの作成
-  const button = document.createElement("button");
-  button.textContent = "削除";
-  //削除ボタンの挙動
-  button.addEventListener("click", () => {
-    //削除内容を受け取る
-    const deleteTarget = button.closest("li");
-    //削除する
-    document.getElementById("memo-list").removeChild(deleteTarget);
+  p.innerText = text;
+
+  // 削除ボタンを作成、クリックイベントを設定
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // alert("deleted");
+    document.getElementById("memo-list").removeChild(li);
+
+    // const deleteTarget = deleteButton.closest("li");
+    // document.getElementById("memo-list").removeChild(deleteTarget);
   });
-  ///divタグに要素を追加
-  div.appendChild(p);
-  div.appendChild(button);
-  //liタグに要素を追加
-  li.appendChild(div);
-  //作成してまとめたliタグをmemo-listのulタグに追加
+
+  // メモ一覧に追加する
+  div.appendChild(p); // メモの内容
+  div.appendChild(deleteButton); // 削除ボタン
+  li.appendChild(div); // li要素にdivを追加
+
+  // メモの内容を設定する
   document.getElementById("memo-list").appendChild(li);
-}
-// 追加ボタン押下時にonClickAddButton関数を実行するよう登録
+};
+
+// 追加ボタンを取得して、クリックイベントを設定
 document
   .getElementById("add-button")
-  .addEventListener("click", () => onClickAddButton());
+  .addEventListener("click", onClickAddButton);
